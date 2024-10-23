818d5b512baf69ecbe60cb9970b5bf62e4ffd450
akmalhossain <akmalforu@gmail.com>
2024-10-23 18:44:30 +0600

    tailwind added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="flex bg-red-400">
+            <h2>Logo</h2>
+            
+                <a>Home</a>
+                <a>Product</a>
+                <a>Cart</a>
+                <a>$ 500</a>
+            
+        </nav>
+    );
+};
+
+export default Navbar;
